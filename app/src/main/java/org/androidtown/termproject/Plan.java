ed1d905124a88167026e8f8251b4894fbfd0fbf7
dkdlxkwks <dkdlxkwks@naver.com>
2016-11-27 17:34:14 +0900

    Plan ListView add

--- app/src/main/java/org/androidtown/termproject/Plan.java
+++ app/src/main/java/org/androidtown/termproject/Plan.java
@@ -1,7 +1,12 @@
 package org.androidtown.termproject;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 public class Plan extends AppCompatActivity {
 
@@ -9,5 +14,41 @@ public class Plan extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.plan);
+
+        ListView listview ;
+        ListViewAdapter adapter;
+
+        // Adapter 생성
+        adapter = new ListViewAdapter() ;
+
+        // 리스트뷰 참조 및 Adapter달기
+        listview = (ListView) findViewById(R.id.listview1);
+        listview.setAdapter(adapter);
+
+        // 첫 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_account_box_black_36dp),
+                "Box", "Account Box Black 36dp") ;
+        // 두 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_account_box_black_36dp),
+                "Circle", "Account Circle Black 36dp") ;
+        // 세 번째 아이템 추가.
+        adapter.addItem(ContextCompat.getDrawable(this, R.drawable.ic_account_box_black_36dp),
+                "Ind", "Assignment Ind Black 36dp") ;
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View v, int position, long id) {
+                // get item
+                Plan_listview item = (Plan_listview) parent.getItemAtPosition(position);
+
+                String titleStr = item.getTitle();
+                String descStr = item.getDesc();
+                Drawable iconDrawable = item.getIcon();
+
+                // TODO : use item data.
+            }
+        }) ;
+
     }
+
 }
